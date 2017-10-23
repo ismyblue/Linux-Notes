080bacb23f45ea767b4a09eee26c8ccdaa101073
Bruce <ismyblue@163.com>
2017-10-24 00:25:57 +0800

    auto commit

--- shell/gitupload.sh
+++ shell/gitupload.sh
@@ -3,6 +3,17 @@
 #一次性更新本地仓库，并且更新远程仓库
 #author:Bruce ismyblue@163.com
 
+str="auto commit"
+
+if [ $# -ge 1 ] ; then
+for f in $*
+do
+   str=$str" "$f
+done
+
+fi
+
+echo $str
 
 echo -e "\n------------------\n查看工作区状态...\n------------------\n"
 git status
@@ -11,7 +22,7 @@ git add -v --all
 echo -e "\n------------------\n查看工作区状态...\n------------------\n"
 git status
 echo -e "\n------------------\n提交工作状态.备注auto updae...\n------------------\n"
-git commit -m "auto update"
+git commit -m "$str"
 echo -e "\n------------------\n拉取远程仓库更新...\n------------------\n"
 git fetch origin master
 echo -e "\n------------------\n合并远程仓库更新...\n------------------\n"
